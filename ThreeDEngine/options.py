7c42d84cebf2ca7f7d7a40ce421fa63c6ab8c13c
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-07-03 10:53:50 +0400

    fixed an error

--- ThreeDEngine/options.py
+++ ThreeDEngine/options.py
@@ -1,8 +1,9 @@
 from glm import vec2
 
+
 class Options:
     fps = 60
     window_size = vec2(1024, 720)
-    background_color = (0, 0, 0)
+    background_color = (0, 0, 0, 255)
     stroke_width = 1
     render_dist = [10, 500]
